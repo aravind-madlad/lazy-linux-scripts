76e89bb2bf52cd1e9bf16d94706ab1033e726f43
aravind-madlad <77500058+aravind-madlad@users.noreply.github.com>
2021-01-15 21:27:32 +0530

    Update c.sh

--- c.sh
+++ c.sh
@@ -1,4 +1,11 @@
 #!/bin/zsh
+/*
+To alias it under zshrc for global usage, paste the following to your 
+.zshrc config file and the shell script in the home folder.
+
+*alias c='bash ~/c.sh $1'*
+
+*/
 gcc $1.c -o $1
 ./$1
 echo
